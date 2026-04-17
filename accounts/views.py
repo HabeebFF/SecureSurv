98669fb8bb71431c433a7cf35da887a5c95a17a5
HabeebFF <habeebmuftau05@gmail.com>
2026-04-17 17:49:48 +0100

    fixed bug

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAdminUser, AllowAny
 
-from .models import Admin
+from .models import User as Admin
 
 
 class CreateAdminView(APIView):
