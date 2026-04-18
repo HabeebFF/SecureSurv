28cc2564deada80943c6fbfe1b5dd34781958df5
HabeebFF <habeebmuftau05@gmail.com>
2026-04-18 19:00:25 +0100

    fixed cors issue

--- surveillance_system/settings.py
+++ surveillance_system/settings.py
@@ -54,6 +54,7 @@ AUTH_USER_MODEL = 'accounts.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -171,6 +172,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
     "http://localhost:3000",
 ]
